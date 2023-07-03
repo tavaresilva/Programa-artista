bb48ba931b9dff82611aae3c34bd1363fc3b98bd
tavaresilva <137805372+tavaresilva@users.noreply.github.com>
2023-07-03 12:01:10 -0300

    Add files via upload

---
+++ programa-artista/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+  createCanvas(600, 600)
+   background("rgba(255,255,255,0.95)");
+}
+
+function draw() {
+  
+  
+  if(mouseIsPressed){
+    
+     fill("rgb(50,98,219)"); 
+       rect(mouseX, mouseY, 40)
+    }
+   
+  }
+ 
+
